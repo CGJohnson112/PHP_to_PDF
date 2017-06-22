0af794c3e9a623dc6b588552b6977afab8c5ec46
Chris Johnson <chrisjohnson112@yahoo.com>
2017-06-21 22:57:46 -0500

    initial commit

---
+++ pdf-template/welcome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	
+</head>
+<body>
+<div class="container-fluid">
+
+	   Your name is <?php echo $name ?>		<br />
+		and your favorite color is is <?php echo $color ?>	.
+		<br />
+		Your favorite movie is <?php echo $favMovie ?>	.
+		You are here <?php echo $reason ?>. Your favorite computer language is <?php echo $frameLang ?>, and you wanted to comment that <?php echo $comment ?>.	  
+		
+</div>
+	
+
+
+</div>
+	
+</body>
+</html>
